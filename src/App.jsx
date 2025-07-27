4f6ad6ac499aa86f1bd13c80d8d629f287dd76d6
Nikku Ananda <nikkuananda16@gmail.com>
2025-07-27 10:19:28 +0700

    ✅ Fix import case-sensitive Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
-import Navbar from "./components/navbar";
+import Navbar from "./component/navbar";
 
 function App() {
   return (
